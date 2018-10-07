ca95e32ceb630afce6982ee597cfe96b2bc26192
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-07 16:19:46 +0900

    Add exception handling

--- vue-bitcoin-app/index.js
+++ vue-bitcoin-app/index.js
@@ -4,10 +4,15 @@ const app = new Vue({
     bpi: null
   },
   mounted: function() {
-    axios.get("https://api.coindesk.com/v1/bpi/currentprice.json").then(
-      function(response) {
-        this.bpi = response.data.bpi;
-      }.bind(this)
-    );
+    axios
+      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then(
+        function(response) {
+          this.bpi = response.data.bpi;
+        }.bind(this)
+      )
+      .catch(function(error) {
+        console.log(error);
+      });
   }
 });
