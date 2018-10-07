c1d3d55c8d25c5bfceaa779cab1a14be9b4d4dff
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-07 16:14:07 +0900

    Add getting bpi

--- vue-bitcoin-app/index.js
+++ vue-bitcoin-app/index.js
@@ -1,3 +1,13 @@
 const app = new Vue({
-  el: "#app"
+  el: "#app",
+  data: {
+    bpi: null
+  },
+  mounted: function() {
+    axios.get("https://api.coindesk.com/v1/bpi/currentprice.json").then(
+      function(response) {
+        this.bpi = response.data.bpi;
+      }.bind(this)
+    );
+  }
 });
