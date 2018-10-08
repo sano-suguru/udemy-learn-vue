2e555288bd8b9490be1a6c711471adfb494fc1a5
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-08 21:59:06 +0900

    Learned about wathcers

---
+++ watchers/index.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    message: "Hello Vue.js!"
+  },
+  watch: {
+    message: function(newValue, oldValue) {
+      console.log("new: %s, old: %s", newValue, oldValue);
+    }
+  }
+});
