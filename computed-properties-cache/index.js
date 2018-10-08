3c0e463102ea1479dfaf78310ef932316c39e4aa
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-08 21:47:49 +0900

    Learned about computed properties cache

---
+++ computed-properties-cache/index.js
@@ -0,0 +1,15 @@
+const app = new Vue({
+  el: "#app",
+  computed: {
+    computedNumber: function() {
+      console.log("computed!");
+      return Math.random();
+    }
+  },
+  methods: {
+    methodsNumber: function() {
+      console.log("methods!");
+      return Math.random();
+    }
+  }
+});
