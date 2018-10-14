3be2d5a5867f143e4cc5c28d3a36f5fc0e642be0
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-14 21:12:17 +0900

    Learned about System modifers

---
+++ system-modifers/index.js
@@ -0,0 +1,14 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    message: ""
+  },
+  methods: {
+    clear: function() {
+      this.message = "";
+    },
+    handleClick: function() {
+      alert("shift + click");
+    }
+  }
+});
